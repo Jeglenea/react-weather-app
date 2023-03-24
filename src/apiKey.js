1194ee97b6faf1628e2f4a469558427065a8e43a
jeglenea <jeglenea@gmail.com>
2023-03-24 12:58:35 +0300

    firstCommit

---
+++ src/apiKey.js
@@ -0,0 +1,10 @@
+export const geoAPI = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a282333cdamsh2279041f1b6e648p12920ajsn8229135c9111',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
