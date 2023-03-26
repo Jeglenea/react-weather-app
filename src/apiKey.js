86e1b8476102bde9edbc1e962e56d57161e16ab2
jeglenea <jeglenea@gmail.com>
2023-03-26 21:29:22 +0300

    api key but they will change

--- src/apiKey.js
+++ src/apiKey.js
@@ -1,11 +1,11 @@
 export const geoAPI = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'your-api-key-from-geodb',
+		'X-RapidAPI-Key': 'a282333cdamsh2279041f1b6e648p12920ajsn8229135c9111',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 export const GEO_API ="https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API="https://api.openweathermap.org/data/2.5"
-export const openWeatherAPI="your-api-key-from-openWeather"
+export const openWeatherAPI="e7b4996759fc8566ebb45ec4fdafe981"
