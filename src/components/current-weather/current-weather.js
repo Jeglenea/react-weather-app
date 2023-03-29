7a041fe6d0d0856089c14b22383f0b00f39cf7b9
jeglenea <jeglenea@gmail.com>
2023-03-29 16:34:18 +0300

    beautifully polished app

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -21,7 +21,7 @@ const CurrentWeather = ({data}) => {
                         <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className="parameter-row">
-                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-label">Wind Speed</span>
                         <span className="parameter-value">{data.wind.speed} m/s</span>
                     </div>
                     <div className="parameter-row">
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -15,7 +15,10 @@ const Forecast = ({ data }) => {
     return (
         <>
             <Typography variant="h6" gutterBottom>
-                Hourly Forecast
+                <p className='Header'>Hourly Forecast</p>
+            </Typography>
+            <Typography variant="h7" gutterBottom>
+                <p className='Header-info'>(Times are shown based on your location, not based on the time of the country or city you are searching for)</p>
             </Typography>
             {data.list.slice(0, 7).map((item, idx) => (
                 <Accordion
@@ -24,17 +27,18 @@ const Forecast = ({ data }) => {
                     onChange={() => setSelected(selected === idx ? null : idx)}
                 >
                     <AccordionSummary
+                        className='daily-item'
                         expandIcon={<ExpandMore />}
                         aria-controls={`panel${idx}-content`}
                         id={`panel${idx}-header`}
                     >
-                        <>
-                            <img alt="weather" className="weather-icon" src={`icons/${item.weather[0].icon}.png`} />
+                        <>  
+                            <img alt="weather" className="weather-icon-forecast" src={`icons/${item.weather[0].icon}.png`} />
                             <Typography variant="subtitle1" className='forecast-time'>{forecastTimes[idx]}</Typography>
                             {new Date(item.dt_txt).getDate() !== new Date().getDate() && (
-                                <Typography variant="subtitle2">Next day</Typography>)}
+                                <Typography variant="subtitle2" className='day-info'>Next day</Typography>)}
                             {new Date(item.dt_txt) < new Date() && (
-                                <Typography variant="subtitle2">Passed</Typography>)}
+                                <Typography variant="subtitle2" className='day-info'>Passed</Typography>)}
                             <Typography variant="subtitle2" className='weather-description'>{item.weather[0].description}</Typography>
                             <Typography variant="subtitle2" className='temperature'>{Math.round(item.main.temp)}°C / {Math.round(item.main.feels_like)}°C</Typography>
                         </>
@@ -43,15 +47,15 @@ const Forecast = ({ data }) => {
                         <div className="daily-detail-grid">
                             <div className="daily-detail-grid-item">
                                 <Typography variant="subtitle2">Pressure</Typography>
-                                <Typography variant="subtitle2">{item.main.pressure}</Typography>
+                                <Typography variant="subtitle2">{item.main.pressure} Pa</Typography>
                             </div>
                             <div className="daily-detail-grid-item">
                                 <Typography variant="subtitle2">Humidity</Typography>
-                                <Typography variant="subtitle2">{item.main.humidity}</Typography>
+                                <Typography variant="subtitle2">{item.main.humidity}%</Typography>
                             </div>
                             <div className="daily-detail-grid-item">
                                 <Typography variant="subtitle2">Clouds</Typography>
-                                <Typography variant="subtitle2">{item.clouds.all}% </Typography>
+                                <Typography variant="subtitle2">{item.clouds.all}%</Typography>
                             </div>
                             <div className="daily-detail-grid-item">
                                 <Typography variant="subtitle2">Wind Speed</Typography>
@@ -64,10 +68,6 @@ const Forecast = ({ data }) => {
                             <div className="daily-detail-grid-item">
                                 <Typography variant="subtitle2">Visibility</Typography>
                                 <Typography variant="subtitle2">{(item.visibility / 1000).toFixed(1)} km</Typography>
-                            </div>    
-                            <div className="daily-detail-grid-item">
-                                <Typography variant="subtitle2">UV Index</Typography>
-                                <Typography variant="subtitle2">{item.uvi}</Typography>
                             </div>
                         </div>
                     </AccordionDetails>
